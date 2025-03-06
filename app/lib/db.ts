26d58ba6bb0df68d3c9c03d96f5615ded6b008f1
Harneet <harneetsahi8@gmail.com>
2025-03-06 00:02:29 -0600

    singleton typesafe

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,8 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = globalThis.prisma ?? new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+const prisma = global.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
 
